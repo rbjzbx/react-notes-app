18c3fc06d780429bff11e85d47e1fb34045de01f
sxw <916269658@qq.com>
2025-05-11 14:49:40 +0800

    baseURL

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api', // 后端 API 的基础 URL
+  baseURL: 'http://101.35.128.180:3000/api', // 后端 API 的基础 URL
   withCredentials: true, // 如果需要跨域请求，确保携带 cookie
 });
 
